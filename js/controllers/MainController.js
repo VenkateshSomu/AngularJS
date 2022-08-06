90910e041e94b5383a6c6acb6b2604b138f5908d
Venkatesh Somu <venkatesh9918@gmail.com>
2022-08-06 15:39:25 +0530

    added https feature

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,4 @@
-app.controller("MainController", ["$scope", function($scope){
+app.controller("MainController", ["$scope", "$http", function($scope, $http){
     $scope.todos = {
        title: "Todos!",
        list: ["Learn basics of Angular JS", "Create Sample Apps", "Practice more", "Become pro in AngularJS"]
@@ -7,7 +7,18 @@ app.controller("MainController", ["$scope", function($scope){
         title: "Books!",
         list: ["Book1", "Book2","Book3","Book4"]
     }
+    function isISBN(item){
+        return /^[0-9]*$/.test(item);
+    }
     $scope.addItem = function(itemList, item){
+        if((item.length === 10 || item.length ===13) && isISBN(item)){
+            console.log("item is ISBN")
+            $http.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + item).then(function success(data) {
+                console.log(data);
+            })
+        } else {
+            console.log("Not an ISBN number");
+        }
         itemList.push(item)
     }
 }]);
